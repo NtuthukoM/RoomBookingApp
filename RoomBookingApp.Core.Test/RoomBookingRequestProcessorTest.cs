26c7423a7690204d6d4e77abc9c2a6f187d97c37
Ntuthuko <ntuthukorocks@gmail.com>
2022-08-14 12:38:22 +0200

    Add project files.

---
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,11 @@
+namespace RoomBookingApp.Core.Test
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnRoomBookingRequest()
+        {
+            var processor = new RoomBookingRequestProcessor();
+        }
+    }
+}
