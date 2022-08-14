c08d726f202d6272268404fe3bc7a265e0c184d5
Ntuthuko <ntuthukorocks@gmail.com>
2022-08-14 13:27:59 +0200

    Return result with same values as request Close #1

--- RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
@@ -1,11 +1,34 @@
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
 namespace RoomBookingApp.Core.Test
 {
     public class RoomBookingRequestProcessorTest
     {
         [Fact]
-        public void ShouldReturnRoomBookingRequest()
+        public void ShouldReturnRoomBookingResponseWithValues()
         {
+            //Arrange
+            var request = new RoomBookingRequest()
+            {
+                FullName = "Test name",
+                EmailAddress = "test@example.com",
+                Date = new DateTime(2022, 8, 15)
+            };
+
+
             var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result);
+            //result.ShouldNotBeNull();
+            Assert.Equal(result.FullName, request.FullName);
+            Assert.Equal(result.EmailAddress, request.EmailAddress);
+            Assert.Equal(result.Date, request.Date);
         }
     }
+
 }
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,18 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingResult BookRoom(RoomBookingRequest request)
+        {
+            var result = new RoomBookingResult()
+            {
+                FullName = request.FullName,
+                EmailAddress = request.EmailAddress,
+                Date = request.Date
+            };
+            return result;
+        }
+    }
+}
